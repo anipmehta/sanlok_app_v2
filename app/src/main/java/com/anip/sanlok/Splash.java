7b49a30d21207cd973e2fa3cb55f0dfd1741cd03
Anip Mehta <anipmehta13@gmail.com>
2017-06-18 17:58:23 +0530

    initialization

---
+++ app/src/main/java/com/anip/sanlok/Splash.java
@@ -0,0 +1,17 @@
+package com.anip.sanlok;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anip on 18/06/17.
+ */
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
